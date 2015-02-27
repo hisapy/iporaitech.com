39b0edc53354ff4cc57170e99166d1af0cfb4ca8
hisa <hisa@iporaitech.com>
2015-02-27 19:54:51 -0300

    CSS Animation for background gradient in logo only page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  # Different path for development asssets when precompiling locally
+  config.assets.prefix = "/dev-assets"
 end
