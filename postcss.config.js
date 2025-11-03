cf3d7bcc6e0505f11f962effef059cdd4f28840a
Hisa Ishibashi <hisa@iporaitech.com>
2025-11-03 12:37:18 -0300

    Fix missing responsive Tailwind styles in production build
    Tailwind v4 handles puring automatically, however, the final bundle is larger due to
    how theme variables are handled in this version, according to other users seeing this same
    behavior.
    
    The cssnano plugin has also been removed because the css-minimizer-webpack-plugin already
    uses it underneath it.

--- postcss.config.js
+++ postcss.config.js
@@ -1,29 +1,5 @@
-// import { purgeCSSPlugin } from "@fullhuman/postcss-purgecss";
-
-// const purgecss = require("@fullhuman/postcss-purgecss")({
-//   content: ["./**/*.html"],
-//   defaultExtractor: (content) => content.match(/[\w-/:]*[\w-/:]/g) || [],
-//   // Whitelist .medium-widget styles because the corresponding
-//   // markup is added dynamically by the widget's JavaScript
-//   whitelistPatterns: [/^medium\-widget/],
-//   whitelistPatternsChildren: [/^medium\-widget/],
-// });
-
-/** @type {import('postcss-load-config').Config} */
-const { purgeCSSPlugin } = require("@fullhuman/postcss-purgecss");
-const cssNanoPlugin = require("cssnano");
-
-module.exports = (ctx) => {
+module.exports = () => {
   return {
-    plugins: [
-      require("postcss-preset-env"),
-      require("@tailwindcss/postcss"),
-      ...(ctx.mode === "production"
-        ? [
-            purgeCSSPlugin({ content: ["./**/*.html"] }),
-            cssNanoPlugin({ preset: "default" }),
-          ]
-        : []),
-    ],
+    plugins: [require("postcss-preset-env"), require("@tailwindcss/postcss")],
   };
 };
