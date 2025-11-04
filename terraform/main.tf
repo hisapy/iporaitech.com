5893485242b607ced02b197ac22e31bf91a07f0b
Hisa Ishibashi <hisa@iporaitech.com>
2025-11-04 15:01:31 -0300

    Add Route53 and ACM resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,22 +11,22 @@ provider "aws" {
   }
 }
 
-# resource "aws_route53_zone" "this" {
-#   name    = var.root_domain
-#   comment = "Hosted zone for ${var.project}"
+resource "aws_route53_zone" "this" {
+  name    = var.root_domain
+  comment = "Hosted zone for ${var.project}"
 
-#   lifecycle {
-#     prevent_destroy = true
-#   }
-# }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
 
-# module "dns_cert" {
-#   source = "git::https://github.com/hisapy/terraform-aws-acm-route53-cert.git?ref=v0.0.1"
+module "dns_cert" {
+  source = "git::https://github.com/hisapy/terraform-aws-acm-route53-cert.git?ref=v0.0.1"
 
-#   route53_zone_id = aws_route53_zone.this.zone_id
-#   root_domain     = var.root_domain
-#   subdomains      = ["www"]
-# }
+  route53_zone_id = aws_route53_zone.this.zone_id
+  root_domain     = var.root_domain
+  subdomains      = ["www"]
+}
 
 resource "aws_cloudfront_function" "request_handler" {
   name    = "static_web_request_handler"
@@ -41,9 +41,9 @@ module "static_website" {
   project = var.project
   env     = var.env
 
-  # cdn_aliases     = module.dns_cert.names
-  # acm_cert_arn    = module.dns_cert.acm_cert_arn
-  # route53_zone_id = aws_route53_zone.this.zone_id
+  cdn_aliases     = module.dns_cert.names
+  acm_cert_arn    = module.dns_cert.acm_cert_arn
+  route53_zone_id = aws_route53_zone.this.zone_id
 
   function_association = [{
     event_type   = "viewer-request"
@@ -51,3 +51,44 @@ module "static_website" {
   }]
 }
 
+## Route53 records
+### MX
+resource "aws_route53_record" "mx" {
+  zone_id = aws_route53_zone.this.zone_id
+  name    = var.root_domain
+  type    = "MX"
+  records = var.dns_mx_records
+  ttl     = var.dns_mx_ttl
+}
+
+### Google Site Verification and SPF (Sender Policy Framework)
+### NOTICE this is how to create records with the same name
+resource "aws_route53_record" "gsv_spf" {
+  zone_id = aws_route53_zone.this.zone_id
+  name    = ""
+  type    = "TXT"
+  ttl     = 300
+  records = [
+    var.dns_google_site_verification_txt,
+    var.dns_spf_txt
+  ]
+}
+
+## DKIM (DomainKeys Identified Mail)
+locals {
+  // A string in a TXT record can be up to 255 characters long
+  // To address this constraint, split into 255-character chunks
+  dns_dkim_chunks = [
+    for i in range(0, length(var.dns_dkim_txt), 255) :
+    substr(var.dns_dkim_txt, i, 255)
+  ]
+  // Join the chunks using the string delimiter \"\" 
+  dns_dkim_txt_split = join("\"\"", [for chunk in local.dns_dkim_chunks : chunk])
+}
+resource "aws_route53_record" "dkim" {
+  zone_id = aws_route53_zone.this.zone_id
+  name    = "${var.dns_dkim_selector}._domainkey.${var.root_domain}"
+  type    = "TXT"
+  records = [local.dns_dkim_txt_split]
+  ttl     = var.dns_dkim_ttl
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -21,3 +21,48 @@ variable "root_domain" {
   default     = "iporaitech.com"
 }
 
+variable "dns_google_site_verification_txt" {
+  type        = string
+  description = "DNS TXT record to prove ownership of a Google Workspace domain"
+}
+
+# See https://support.google.com/a/answer/33786
+variable "dns_spf_txt" {
+  type        = string
+  description = "DNS TXT record to set up Sender Policy Framework (SPF)"
+  default     = "v=spf1 include:_spf.google.com ~all"
+}
+
+# These are legacy MX records
+# See: https://support.google.com/a/answer/16004259
+variable "dns_mx_records" {
+  type        = list(string)
+  description = "DNS MX records for Gmail"
+  default = [
+    "1 ASPMX.L.GOOGLE.COM",
+    "5 ALT1.ASPMX.L.GOOGLE.COM",
+    "5 ALT2.ASPMX.L.GOOGLE.COM",
+    "10 ALT3.ASPMX.L.GOOGLE.COM",
+    "10 ALT4.ASPMX.L.GOOGLE.COM"
+  ]
+}
+variable "dns_mx_ttl" {
+  type    = number
+  default = 3600
+}
+
+variable "dns_dkim_selector" {
+  type        = string
+  description = "DKIM selector, issued by the email service provider"
+  default     = "google"
+}
+variable "dns_dkim_txt" {
+  type        = string
+  description = "DNS TXT value for the DKIM record"
+}
+variable "dns_dkim_ttl" {
+  type    = number
+  default = 300
+}
+
+
